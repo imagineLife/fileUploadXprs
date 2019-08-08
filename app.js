dae0dad2602ab4b3ba483c49b8643b7ae84cd9b9
Eric Laursen <ericjlaursen@gmail.com>
2019-08-07 21:08:41 -0400

    prep ejs && root route

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ const path = require('path')
 const app = express()
 const PORT = 3000;
 
+//EJS
+app.set('view engine', 'ejs')
+
+//Public folder
+app.use(express.static('./public'))
+
+//root route
+app.get('/', (req, res) => {
+	res.render('index')
+})
+
 app.listen(PORT, () => {
 	console.log(`node/express server running on ${PORT}`);
 })
