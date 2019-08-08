9958ba29380e99cb4ba2870e9aeb6396e3f9f066
Eric Laursen <ericjlaursen@gmail.com>
2019-08-07 21:07:18 -0400

    express startup

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ const express = require('express')
 const multer = require('multer')
 const ejs = require('ejs')
 const path = require('path')
+
+//init app
+const app = express()
+const PORT = 3000;
+
+app.listen(PORT, () => {
+	console.log(`node/express server running on ${PORT}`);
+})
