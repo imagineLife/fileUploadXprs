308b4e7cbc2f834cb055b81759608765e58216f6
Eric Laursen <ericjlaursen@gmail.com>
2019-08-08 06:05:40 -0400

    multer parts && uploadFile endpoint

--- app.js
+++ app.js
@@ -3,6 +3,27 @@ const multer = require('multer')
 const ejs = require('ejs')
 const path = require('path')
 
+//set a storage Engine
+const storageLocation = multer.diskStorage({
+	//storage location
+	destination: './public/uploads/',
+
+	//
+	filename: function(req,file,cb){
+
+		//null the error
+		//fieldname-timestamp
+		//.extname() gets the extension USING NODE!!
+		let newFileName = `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}}`
+		cb(null, newFileName)
+	}
+})
+
+//Multer uploader
+const upload = multer({
+	storage: storageLocation
+}).single(`myImage`)
+
 //init app
 const app = express()
 const PORT = 3000;
@@ -12,6 +33,13 @@ app.set('view engine', 'ejs')
 
 //Public folder
 app.use(express.static('./public'))
+
+//post endpoint
+app.post('/uploadFile', (req,res) => {
+	
+	//dummy check
+	res.send('miCCheck')
+})
 
 //root route
 app.get('/', (req, res) => {
