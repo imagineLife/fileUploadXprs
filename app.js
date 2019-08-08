92eaa7c7ab20720d19f3999faab1fb081f5833dd
Eric Laursen <ericjlaursen@gmail.com>
2019-08-08 06:40:22 -0400

    denies non-images

--- app.js
+++ app.js
@@ -14,15 +14,41 @@ const storageLocation = multer.diskStorage({
 		//null the error
 		//fieldname-timestamp
 		//.extname() gets the extension USING NODE!!
-		let newFileName = `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}}`
+		let newFileName = `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
 		cb(null, newFileName)
 	}
 })
 
 //Multer uploader
-const upload = multer({
-	storage: storageLocation
+const uploadFile = multer({
+	storage: storageLocation,
+
+	// limits: {fileSize: numberOfBytes IE 1000000} //sets a file-size limit
+	
+	//assurance of IMAGE-ONLY type
+	fileFilter: function(req,file,cb){
+		checkFileType(file,cb)
+	}
+
 }).single(`myImage`)
+
+//can validate details about the file
+const checkFileType = (file, cb) => {
+
+	//checks file-type via file-extension
+	const fileTypesExp = /jpeg|jpg|png|gif/
+	let extName = fileTypesExp.test(path.extname(file.originalname))
+	extName = extName ? extName.toLowerCase() : extName;
+	//check Mime-Type
+	const mimetype = fileTypesExp.test(file.mimetype)
+
+	if(mimetype && extName){
+		return cb(null, true)
+	}else{
+		//sends error, handled in the uploadFile cb
+		cb('Error: Images only!!')
+	}
+}
 
 //init app
 const app = express()
@@ -37,8 +63,17 @@ app.use(express.static('./public'))
 //post endpoint
 app.post('/uploadFile', (req,res) => {
 	
-	//dummy check
-	res.send('miCCheck')
+	uploadFile(req,res, (err) => {
+		if(!err){
+			console.log('FILE!!')
+			console.log(req.file)
+			res.send('miCCheck')
+		}else{
+			res.render('index', {
+				msg: err
+			})
+		}
+	})
 })
 
 //root route
